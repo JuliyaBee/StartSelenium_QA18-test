abaef9bc45ef6c9257ca2769267dab116c7a07ee
JuliyaBee <juliyabee@gmail.com>
2023-02-21 21:03:29 +0200

    cw_5

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestClass {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition() {
+        System.out.println("Opening web-site");
+        wd = new ChromeDriver();
+        //wd.get("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/home");
+        //wd.navigate().forward();
+        //wd.navigate().back();
+    }
+
+    @Test
+    public void testLogin() {
+      WebElement element= wd.findElement(By.tagName("a"));
+     // wd.findElement(By.tagName("a"));
+      List<WebElement>elements=wd.findElements(By.tagName("a"));
+        System.out.println(elements.size());
+        By.
+    }
+
+    @AfterMethod
+    public void postCondition() {
+      //  wd.close();
+       wd.quit();
+
+    }
+}
