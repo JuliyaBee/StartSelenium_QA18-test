e1d91e62bbe7d77416fc1a008f0e346099f78ec3
JuliyaBee <juliyabee@gmail.com>
2023-02-25 23:59:43 +0200

    HW_6

--- src/test/java/SeleniumStart.java
+++ src/test/java/SeleniumStart.java
@@ -17,39 +17,55 @@ public class SeleniumStart {
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
         //wd.get("https://telranedu.web.app/home");
-        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/login");
         //wd.navigate().forward();
         //wd.navigate().back();
     }
 
     @Test
     public void testLogin() {
-        WebElement element = wd.findElement(By.tagName("a"));
-
-        List<WebElement> elements = wd.findElements(By.tagName("a"));
-        System.out.println(elements.size());
-        List<WebElement> divs = wd.findElements(By.tagName("div"));
-        System.out.println(divs.size());
-        WebElement div = wd.findElement(By.tagName("div"));
-        WebElement active = wd.findElement(By.className("active"));
-        System.out.println(active);
-        List<WebElement> actives = wd.findElements(By.className("active"));
-        System.out.println(actives.size());
-        WebElement login = wd.findElement(By.linkText("LOGIN"));
-        System.out.println(login);
-        List<WebElement> bo = wd.findElements(By.partialLinkText("BO"));
-        System.out.println((bo.size()));
-        WebElement h1 = wd.findElement(By.tagName("h1"));
-        System.out.println(h1);
-        List<WebElement>home = wd.findElements(By.partialLinkText("HOME"));
-        System.out.println(home.size());
+//        WebElement element = wd.findElement(By.tagName("a"));
+//        WebElement form = wd.findElement(By.tagName("form"));
+//        WebElement root = wd.findElement(By.id("root"));
+//        wd.findElement(By.className("login_login__3EHKB"));
+//        wd.findElement(By.linkText("Login"));
+//        wd.findElement(By.name("password"));
+//
+//
+//        List<WebElement> elements = wd.findElements(By.tagName("a"));
+//        System.out.println(elements.size());
+//        List<WebElement> divs = wd.findElements(By.tagName("div"));
+//        System.out.println(divs.size());
+//        WebElement div = wd.findElement(By.tagName("div"));
+//        WebElement active = wd.findElement(By.className("active"));
+//        System.out.println(active);
+//        List<WebElement> actives = wd.findElements(By.className("active"));
+//        System.out.println(actives.size());
+//        WebElement login = wd.findElement(By.linkText("LOGIN"));
+//        System.out.println(login.getText());
+//        System.out.println(login);
+//        List<WebElement> bo = wd.findElements(By.partialLinkText("BO"));
+//        System.out.println((bo.size()));
+//        WebElement h1 = wd.findElement(By.tagName("h1"));
+//        System.out.println(h1.getLocation());
+//        System.out.println(h1);
+//        List<WebElement>home = wd.findElements(By.partialLinkText("HOME"));
+//        System.out.println(home.size());
+        WebElement element = wd.findElement(By.cssSelector("a"));
+        WebElement form = wd.findElement(By.cssSelector("form"));
+        WebElement login_login__3EHKB = wd.findElement(By.cssSelector(".login_login__3EHKB"));
+        WebElement login = wd.findElement(By.cssSelector("[href='/login']"));
+        WebElement password = wd.findElement(By.cssSelector("[placeholder='password']"));
+        WebElement login_style = wd.findElement(By.cssSelector("[style*='border: 1px solid black;'] "));
+        WebElement navbar = wd.findElement(By.cssSelector("[class^='navbar']"));
+        WebElement style = wd.findElement(By.cssSelector("[style$='color: white;']"));
 
     }
 
     @AfterMethod
     public void postCondition() {
         //  wd.close();
-        wd.quit();
+        // wd.quit();
 
     }
 }
--- src/test/java/SeleniumStartHW.java
+++ src/test/java/SeleniumStartHW.java
@@ -12,13 +12,14 @@ public class SeleniumStartHW {
 
 
     WebDriver wd;
+    WebDriver wd1;
 
     @BeforeMethod
     public void preCondition() {
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
         //   wd.get("https://ilcarro.web.app/search");
-        wd.navigate().to("https://ilcarro.web.app/search");
+        wd.navigate().to("https://telranedu.web.app/login");
         //  wd.navigate().forward();
         //  wd.navigate().back();
 
@@ -26,15 +27,46 @@ public class SeleniumStartHW {
 
     @Test
     public void testLogin() {
-        WebElement element = wd.findElement(By.tagName("a"));
-        List<WebElement> elements = wd.findElements(By.tagName("a"));
-        System.out.println((elements.size()));
+//        WebElement container = wd.findElement(By.className("container"));
+//        System.out.println(container.getTagName());
+//        System.out.println(container.getAttribute("id"));
+//        WebElement div =wd.findElement(By.tagName("div"));
+//        System.out.println(div.getText());
+//        List<WebElement>divs = wd.findElements(By.tagName("div"));
+//        System.out.println(divs.size());
+//                WebElement element = wd.findElement(By.tagName("a"));
+//        List<WebElement> elements = wd.findElements(By.tagName("a"));
+//        System.out.println((elements.size()));
+//       WebElement active =  wd.findElement(By.className("active"));
+//        System.out.println(active.getTagName());
+//       WebElement phone = wd.findElement(By.tagName("h1"));
+//        System.out.println(phone.getText());
+//       WebElement log = wd.findElement(By.linkText("LOGIN"));
+//        System.out.println(log.getAttribute("login"));
 
+        WebElement container = wd.findElement(By.cssSelector(".container"));
+        WebElement div = wd.findElement(By.cssSelector("div"));
+        WebElement a = wd.findElement(By.cssSelector("a"));
+        List<WebElement> divs = wd.findElements(By.cssSelector("div"));
+        List<WebElement> elements = wd.findElements(By.cssSelector("a"));
+        WebElement active = wd.findElement(By.cssSelector(".active"));
+        WebElement log = wd.findElement(By.cssSelector("[href='/login']"));
+        WebElement password = wd.findElement(By.cssSelector("[name='password']"));
+        WebElement root = wd.findElement(By.cssSelector("#root"));
+        WebElement element = wd.findElement(By.cssSelector("a"));
+        WebElement form = wd.findElement(By.cssSelector("form"));
+        WebElement login_login__3EHKB = wd.findElement(By.cssSelector(".login_login__3EHKB"));
+        WebElement login = wd.findElement(By.cssSelector("[href='/login']"));
+        WebElement login_style = wd.findElement(By.cssSelector("[style*='border: 1px solid black;'] "));
+        WebElement navbar = wd.findElement(By.cssSelector("[class^='navbar']"));
+        WebElement style = wd.findElement(By.cssSelector("[style$='color: white;']"));
+        
     }
 
     @AfterMethod
     public void postCondition() {
-      //  wd.close();
+        System.out.println("End postCondition");
+        //  wd.close();
         wd.quit();
     }
 }
