939d09c777d4be8ef74ff905392c90647dffb4f1
JuliyaBee <juliyabee@gmail.com>
2023-03-02 18:01:27 +0200

    HW_7

--- src/test/java/SeleniumStartHW.java
+++ src/test/java/SeleniumStartHW.java
@@ -12,7 +12,8 @@ public class SeleniumStartHW {
 
 
     WebDriver wd;
-    WebDriver wd1;
+
+
 
     @BeforeMethod
     public void preCondition() {
@@ -44,23 +45,41 @@ public class SeleniumStartHW {
 //       WebElement log = wd.findElement(By.linkText("LOGIN"));
 //        System.out.println(log.getAttribute("login"));
 
-        WebElement container = wd.findElement(By.cssSelector(".container"));
-        WebElement div = wd.findElement(By.cssSelector("div"));
-        WebElement a = wd.findElement(By.cssSelector("a"));
-        List<WebElement> divs = wd.findElements(By.cssSelector("div"));
-        List<WebElement> elements = wd.findElements(By.cssSelector("a"));
-        WebElement active = wd.findElement(By.cssSelector(".active"));
-        WebElement log = wd.findElement(By.cssSelector("[href='/login']"));
-        WebElement password = wd.findElement(By.cssSelector("[name='password']"));
-        WebElement root = wd.findElement(By.cssSelector("#root"));
-        WebElement element = wd.findElement(By.cssSelector("a"));
-        WebElement form = wd.findElement(By.cssSelector("form"));
-        WebElement login_login__3EHKB = wd.findElement(By.cssSelector(".login_login__3EHKB"));
-        WebElement login = wd.findElement(By.cssSelector("[href='/login']"));
-        WebElement login_style = wd.findElement(By.cssSelector("[style*='border: 1px solid black;'] "));
-        WebElement navbar = wd.findElement(By.cssSelector("[class^='navbar']"));
-        WebElement style = wd.findElement(By.cssSelector("[style$='color: white;']"));
-        
+//        WebElement container = wd.findElement(By.cssSelector(".container"));
+//        WebElement div = wd.findElement(By.cssSelector("div"));
+//        WebElement a = wd.findElement(By.cssSelector("a"));
+//        List<WebElement> divs = wd.findElements(By.cssSelector("div"));
+//        List<WebElement> elements = wd.findElements(By.cssSelector("a"));
+//        WebElement active = wd.findElement(By.cssSelector(".active"));
+//        WebElement log = wd.findElement(By.cssSelector("[href='/login']"));
+//        WebElement password = wd.findElement(By.cssSelector("[name='password']"));
+//        WebElement root = wd.findElement(By.cssSelector("#root"));
+//        WebElement element = wd.findElement(By.cssSelector("a"));
+//        WebElement form = wd.findElement(By.cssSelector("form"));
+//        WebElement login_login__3EHKB = wd.findElement(By.cssSelector(".login_login__3EHKB"));
+//        WebElement login = wd.findElement(By.cssSelector("[href='/login']"));
+//        WebElement login_style = wd.findElement(By.cssSelector("[style*='border: 1px solid black;'] "));
+//        WebElement navbar = wd.findElement(By.cssSelector("[class^='navbar']"));
+//        WebElement style = wd.findElement(By.cssSelector("[style$='color: white;']"));
+        wd.findElement(By.xpath("//[@class='container']"));
+        wd.findElement(By.xpath("//div[@id='root' and @class='container']"));
+        wd.findElement(By.xpath("//*div"));
+        wd.findElement(By.xpath("//*a"));
+        wd.findElement(By.xpath("//*div//a"));
+        wd.findElement(By.xpath("//*div//a[@class='active']"));
+        wd.findElement(By.xpath("//*div//a[@href='/login']"));
+        wd.findElement(By.xpath("//*div[@class='navbar-component_nav__1X_4m']/a[3]"));
+        wd.findElement(By.xpath("//input[@name='password']"));
+        wd.findElement(By.xpath("//*div[@class='login_login__3EHKB']/form/input[2]"));
+        wd.findElement(By.xpath("//*div[@id='root']"));
+
+        wd.findElement(By.xpath("//div[@class='login_login__3EHKB'/form"));
+        wd.findElement(By.xpath("//input[@name='email']/.."));
+
+        wd.findElement(By.xpath("//a[start-with@style,'border: 1px solid black;'] "));
+        wd.findElement(By.xpath("//div[start-with@class,'navbar']"));
+        wd.findElement(By.xpath("//a[contains([.,'border: 1px solid black; background-color: black; color: white;']"));
+
     }
 
     @AfterMethod
